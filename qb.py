9133edf9489d3d9a59961a10af066f6eafde2966
Evan Wappel <ewappel@gmail.com>
2020-12-17 19:32:47 +0000

    sync

---
+++ qb.py
@@ -0,0 +1,13 @@
+def qb_df(realm, token, table_id, query):
+    # import requests
+    # url = "https://api.quickbase.com/v1/fields?tableId=" + table_id
+    # payload = {}
+    # headers = {
+    #   'QB-Realm-Hostname': realm,
+    #   'Authorization': token,
+    #   'Content': 'application.json'
+    # }
+    return 'test'
+
+def func2():
+    return 'ok'
